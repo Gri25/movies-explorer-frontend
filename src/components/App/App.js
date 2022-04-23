d69316629742846fa0ad659c5862e7030a7497d5
Gri25 <grigorijabrosimov@gmail.com>
2022-04-24 02:01:31 +0500

    feat: регистрация

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,13 +5,21 @@ import PageNotFound from "../PageNotFound/PageNotFound";
 import Profile from "../Profile/Profile";
 import Register from "../Register/Register";
 import SavedMovies from "../SavedMovies/SavedMovies";
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
 import Movies from "../Movies/Movies";
 import Main from "../Main/Main";
-import moviesApi from "../../utils/MoviesApi"
+import moviesApi from "../../utils/MoviesApi";
+import ProtectedRoute from "../ProtectedRoute";
+import * as auth from "../../utils/auth.js";
 
 function App() {
+  const [loggedIn, setLoggedIn] = useState(false);
   const [isCloseShortsFilms, setIsCloseShortsFilms] = useState(false);
+  const [userData, setUserData] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
 
   function handleEditShortsFilms() {
     setIsCloseShortsFilms(true);
@@ -24,23 +32,90 @@ function App() {
 
   useEffect(() => {
     moviesApi
-    .getCardMovies()
+      .getCardMovies()
       .then((data) => {
         setCards(data);
-        console.log(data)
+        console.log(data);
       })
       .catch((err) => {
         console.log(err);
       });
-    }
-    , []);
+  }, []);
+
+  const handleRegister = ({ name, email, password }) => {
+    auth
+      .register({ name, email, password })
+      .then((data) => {
+        if (data) {
+          setUserData({
+            name,
+            email,
+            password,
+          });
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
 
   return (
     <>
       <Switch>
-        <Route exact path="/">
+        <ProtectedRoute
+          userData={userData}
+          path="/movies"
+          loggedIn={loggedIn}
+          component={Movies}
+          isOpen={isCloseShortsFilms}
+          onEditFilms={handleEditShortsFilms}
+          onClose={closeShortsFilms}
+          cards={cards}
+        >
+          {" "}
+        </ProtectedRoute>
+        <ProtectedRoute
+          userData={userData}
+          path="/saved-movies"
+          loggedIn={loggedIn}
+          component={SavedMovies}
+          isOpen={isCloseShortsFilms}
+          onEditFilms={handleEditShortsFilms}
+          onClose={closeShortsFilms}
+        >
+          {" "}
+        </ProtectedRoute>
+        <ProtectedRoute path="/profile" loggedIn={loggedIn} component={Profile}>
+          {" "}
+        </ProtectedRoute>
+
+        <Route path="/main">
           <Main />
         </Route>
+
+        <Route path="/signin">
+          <Login />
+        </Route>
+        <Route path="/signup">
+          <Register onRegister={handleRegister} />
+        </Route>
+        <Route path="/error">
+          <PageNotFound />
+        </Route>
+        <Route path="/burger">
+          <BurgerMenu />
+        </Route>
+        <Route exact path="/">
+          {loggedIn ? <Redirect to="/movies" /> : <Redirect to="/main" />}
+        </Route>
+      </Switch>
+    </>
+  );
+}
+
+export default App;
+
+/*
         <Route path="/movies">
           <Movies
             isOpen={isCloseShortsFilms}
@@ -49,31 +124,16 @@ function App() {
             cards={cards}
           />
         </Route>
+
         <Route path="/saved-movies">
           <SavedMovies
             isOpen={isCloseShortsFilms}
             onEditFilms={handleEditShortsFilms}
             onClose={closeShortsFilms}
+          </Route>
           />
-        </Route>
-        <Route path="/profile">
+
+                  <Route path="/profile">
           <Profile />
         </Route>
-        <Route path="/signin">
-          <Login />
-        </Route>
-        <Route path="/signup">
-          <Register />
-        </Route>
-        <Route path="/error">
-          <PageNotFound />
-        </Route>
-        <Route path="/burger">
-          <BurgerMenu />
-        </Route>
-      </Switch>
-    </>
-  );
-}
-
-export default App;
+*/
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -2,27 +2,72 @@ import React from "react";
 import logo from "../../images/logo.svg";
 import { Link } from "react-router-dom";
 
-function Register() {
+function Register({ onRegister }) {
+  const [data, setData] = React.useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setData({
+      ...data,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    //  if (data.password) {
+    //если что сотри это условие!!!!!!
+    const { name, email, password } = data;
+    onRegister({ name, email, password });
+  };
+
   return (
     <section className="register">
       <Link to="/" className="register__logo-container">
         <img src={logo} className="register__logo" alt="Лого" />
       </Link>
       <h2 className="register__text">Добро пожаловать!</h2>
+      <form onSubmit={handleSubmit} className="register__form-section">
       <div className="register__input-section">
         <p className="register__input-text">Имя</p>
-        <input className="register__input-info" />
+        <input
+          id="name"
+          name="name"
+          type="string"
+          value={data.name}
+          onChange={handleChange}
+          className="register__input-info"
+        />
       </div>
       <div className="register__input-section">
         <p className="register__input-text">E-mail</p>
-        <input className="register__input-info" />
+        <input
+          id="email"
+          name="email"
+          type="email"
+          value={data.email}
+          onChange={handleChange}
+          className="register__input-info"
+        />
       </div>
       <div className="register__input-section-red">
         <p className="register__input-text">Пароль</p>
-        <input className="register__input-red" type="password" />
+        <input
+          id="password"
+          name="password"
+          type="password"
+          value={data.password}
+          onChange={handleChange}
+          className="register__input-red"
+        />
       </div>
       <span className="register__input-error">Что-то пошло не так...</span>
-      <button className="register__button-reg">Зарегистрироваться</button>
+      <button type="submit" className="register__button-reg">Зарегистрироваться</button>
+      </form>
       <div className="register__information">
         <p className="register__question">Уже зарегистрированы?</p>
         <Link to="/signin" className="register__button-log">
